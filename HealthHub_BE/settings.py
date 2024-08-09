641494c0b6ad445d3e607bbfd2ba342c0dc209b1
David Bergqvist <David.bergqvist@gmail.com>
2024-08-09 08:30:02 +0000

    updated settings, temporary; CORS_ALLOW_ALL_ORIGINS = True

--- HealthHub_BE/settings.py
+++ HealthHub_BE/settings.py
@@ -96,9 +96,10 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
     "http://localhost:3000",
     "https://3000-davidb3rgqvist-project5-c890dyzonnb.ws.codeinstitute-ide.net",
     "https://health-hub-a58a68d5575e.herokuapp.com",
